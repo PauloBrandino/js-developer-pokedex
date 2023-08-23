bb783fe7bb702466db1fa0e6f5630dba787e7f29
PauloBrandino <contatobrandino@gmail.com>
2023-08-23 19:12:21 -0300

    refatora funções da requisição da api. Adiciona campo para filtro no html. Add feature de filtrar pokemon pelo typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-
+const filterPokemon = document.getElementById('pokemonType')
+    
 const maxRecords = 151
 const limit = 10
 let offset = 0;
+let pokemons = []
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -23,25 +25,58 @@ function convertPokemonToLi(pokemon) {
     `
 }
 
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+function getFilteredPokemons(pokemons) {
+    return pokemons.filter((pokemon) => pokemon.type == filterPokemon.value)
+}
+
+async function getPokemonItens(offset, limit) {
+    return pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        return pokemons
+            // const newHtml = pokemons.map(convertPokemonToLi).join('')
+            // pokemonList.innerHTML += newHtml
     })
 }
 
-loadPokemonItens(offset, limit)
+async function loadPokemonInHtml(pokemons) {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+}
 
-loadMoreButton.addEventListener('click', () => {
+async function addMorePokemonInList() {
     offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+        const qtdRecordsWithNexPage = offset + limit
+    
+        if (qtdRecordsWithNexPage >= maxRecords) {
+            const newLimit = maxRecords - offset
+            const getPokemons = await getPokemonItens(offset, newLimit)
+            getPokemons.forEach((pokemon) => pokemons.push(pokemon))
+            loadPokemonInHtml(pokemons)
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+            loadMoreButton.parentElement.removeChild(loadMoreButton)
+        } else {
+            const getPokemons = await getPokemonItens(offset, limit)
+            getPokemons.forEach((pokemon) => pokemons.push(pokemon))
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+            loadPokemonInHtml(pokemons)
+        }
+}
+
+filterPokemon.addEventListener('change', function () {
+    window.location.search
+    if (filterPokemon.value != 'all') {
+        pokemonList.innerHTML = ''
+        loadPokemonInHtml(getFilteredPokemons(pokemons))
     } else {
-        loadPokemonItens(offset, limit)
+        pokemonList.innerHTML = ''
+        loadPokemonInHtml(pokemons)
     }
 })
+
+window.onload = async () => {
+    pokemons = await getPokemonItens(offset, limit)
+    
+    loadMoreButton.addEventListener('click', async () => {
+        addMorePokemonInList()
+        pokemonList.innerHTML = ''
+    })
+    loadPokemonInHtml(pokemons)
+}
